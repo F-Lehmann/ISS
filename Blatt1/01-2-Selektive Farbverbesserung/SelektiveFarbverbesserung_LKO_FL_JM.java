37e417df9ca4e1cc5aff95a5de3d5a7e09a09b89
Jan M <s6jamanh@uni-bonn.de>
2020-11-03 18:41:43 +0100

    Blatt 1 Aufgabe 2 Selektive Farbverbesserung

---
+++ Blatt1/01-2-Selektive Farbverbesserung/SelektiveFarbverbesserung_LKO_FL_JM.java
@@ -0,0 +1,51 @@
+import itb2.filter.AbstractFilter;
+import itb2.filter.RequireImageType;
+import itb2.image.Image;
+import itb2.image.ImageFactory;
+import itb2.image.RgbImage;
+
+/**
+ * verstärkt einen RGB-kanal um einen wert zwischen 0 und 10
+ * 
+ * @author Leo Kyster Oerter, Felix Lehmann, Jan Manhillen
+ */
+
+@RequireImageType(RgbImage.class)
+public class SelektiveFarbverbesserung_LKO_FL_JM extends AbstractFilter {
+
+	public SelektiveFarbverbesserung_LKO_FL_JM() {
+		properties.addOptionProperty("Kanal", "R", "R", "G", "B");
+		properties.addRangeProperty("Faktor", 5, 0, 1, 10);
+	}
+
+	@Override
+	public Image filter(Image input) {
+		Image output = ImageFactory.getPrecision(input).rgb(input.getSize());
+
+		if (properties.getOptionProperty("Kanal") == "R") {
+			for (int col = 0; col < input.getWidth(); col++) {
+				for (int row = 0; row < input.getHeight(); row++) {
+					output.setValue(col, row, properties.getRangeProperty("Faktor") * input.getValue(col, row, RgbImage.RED),
+							input.getValue(col, row, RgbImage.GREEN), input.getValue(col, row, RgbImage.BLUE));
+				}
+			}
+		} else if (properties.getOptionProperty("Kanal") == "G") {
+			for (int col = 0; col < input.getWidth(); col++) {
+				for (int row = 0; row < input.getHeight(); row++) {
+					output.setValue(col, row, input.getValue(col, row, RgbImage.RED),
+							properties.getRangeProperty("Faktor") * input.getValue(col, row, RgbImage.GREEN),
+							input.getValue(col, row, RgbImage.BLUE));
+				}
+			}
+		} else {
+			for (int col = 0; col < input.getWidth(); col++) {
+				for (int row = 0; row < input.getHeight(); row++) {
+					output.setValue(col, row, input.getValue(col, row, RgbImage.RED),
+							input.getValue(col, row, RgbImage.GREEN),
+							properties.getRangeProperty("Faktor") * input.getValue(col, row, RgbImage.BLUE));
+				}
+			}
+		}
+		return output;
+	}
+}
