9e83827674e9c5aa7242a7039fe89ef10ea467b5
Jan M <s6jamanh@uni-bonn.de>
2020-11-10 16:15:41 +0100

    Blatt 2 Aufgabe 1 Gamma-Korrektur

---
+++ Blatt2/GammaKorrektur_LKO_FL_JM.java
@@ -0,0 +1,38 @@
+import itb2.filter.AbstractFilter;
+import itb2.filter.RequireImageType;
+import itb2.image.GrayscaleImage;
+import itb2.image.Image;
+import itb2.image.ImageFactory;
+
+/**
+ * führt eine gamma-korrektur mir dem vom nutzer eingegebenen wert aus
+ * 
+ * @author Leo Kyster Oerter, Felix Lehmann, Jan Manhillen
+ */
+
+@RequireImageType(GrayscaleImage.class)
+public class GammaKorrektur_LKO_FL_JM extends AbstractFilter {
+
+	public GammaKorrektur_LKO_FL_JM() {
+		properties.addDoubleProperty("γ", 1);
+	}
+
+	@Override
+	public Image filter(Image input) {
+
+		Image output = ImageFactory.getPrecision(input).gray(input.getSize());
+
+		for (int col = 0; col < input.getWidth(); col++) {
+			for (int row = 0; row < input.getHeight(); row++) {
+
+				int newValue = Math.toIntExact(Math.round(256 * Math.pow((input.getValue(col, row, GrayscaleImage.GRAYSCALE) / 255),
+						properties.getDoubleProperty("γ"))));
+
+				output.setValue(col, row, newValue);
+			}
+		}
+
+		return output;
+	}
+
+}
